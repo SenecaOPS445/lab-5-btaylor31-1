9bf3ebe565c7dad7c54b2eeabb2e057cbd636618
Bakari Taylor <btaylor31@myseneca.ca>
2025-02-14 19:39:26 -0500

    Updating lab5

--- lab5b.py
+++ lab5b.py
@@ -1,64 +1,43 @@
 #!/usr/bin/env python3
 # Author ID: btaylor31
 
-def add(a, b):
-    try:
-        # Inputs converted to integers
-        a = int(a)
-        b = int(b)
-        return a + b
-    except ValueError:
-        return 'error: could not add numbers'
 
+def read_file_string(file_name):
+    with open(file_name, 'r') as f:
+        return f.read()
 
-def read_file(filename):
-    try:
-        with open(filename, 'r') as file:
-            lines = file.readlines()
-        return lines
-    except FileNotFoundError:
-        return 'error: could not read file'
+def read_file_list(file_name):
+    with open(file_name, 'r') as f:
+        return [line.strip() for line in f.readlines()]
 
+def append_file_string(file_name, string_of_lines):
+    with open(file_name, 'a') as f:
+        f.write(string_of_lines)
 
-def read_file_string(filename):
-    try:
-        with open(filename, 'r') as file:
-            return file.read()  
-    except FileNotFoundError:
-        return 'error: could not read file'
+def write_file_list(file_name, list_of_lines):
+    with open(file_name, 'w') as f:
+        for line in list_of_lines:
+            f.write(line + '\n')
 
+def copy_file_add_line_numbers(file_name_read, file_name_write):
+    with open(file_name_read, 'r') as fr, open(file_name_write, 'w') as fw:
+        for i, line in enumerate(fr, start=1):
+            fw.write(f"{i}:{line.strip()}\n")
 
-def read_file_list(filename):
-    try:
-        with open(filename, 'r') as file:
-            lines = file.readlines()
-        lines = [line.strip() for line in lines]
-        return lines
-    except FileNotFoundError:
-        return 'error: could not read file'
+# Main code block
+if __name__ == '__main__':
+    file1 = 'seneca1.txt'
+    file2 = 'seneca2.txt'
+    file3 = 'seneca3.txt'
+    string1 = 'First Line\nSecond Line\nThird Line\n'
+    list1 = ['Line 1', 'Line 2', 'Line 3']
 
-def append_file_string(filename, string_to_append):
-    try:
-        with open(filename, 'a') as file:
-            file.write(string_to_append)  # Append 
-    except FileNotFoundError:
-        return 'error: could not write to file'
+    
+    append_file_string(file1, string1)
+    print(read_file_string(file1))
 
-def write_file_list(filename, list_to_write):
-    try:
-        with open(filename, 'w') as file:  # File is opened in write mode
-            for item in list_to_write:
-                file.write(item + '\n')  # Add new line to each item with write function
-    except FileNotFoundError:
-        return 'error: could not write to file'
+    write_file_list(file2, list1)
+    print(read_file_string(file2))
 
-def copy_file_add_line_numbers(input_filename, output_filename):
-    try:
-        with open(input_filename, 'r') as infile:
-            with open(output_filename, 'w') as outfile:
-                line_number = 1
-                for line in infile:
-                    outfile.write(f"{line_number}:{line.strip()}\n")
-                    line_number += 1
-    except FileNotFoundError:
-        return 'error: could not read or write file'
+    copy_file_add_line_numbers(file2, file3)
+    print(read_file_string(file3))
